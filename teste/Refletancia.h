88c2ba67221f571eafb8548fe1c8666a23c7bf38
ithaelly <ithaellymykarlla@gmail.com>
2017-11-07 12:03:38 -0300

    Fiz alterações nos cometários explicativos do código

--- teste/Refletancia.h
+++ teste/Refletancia.h
@@ -1,5 +1,5 @@
 struct Refletancia{             //struct e a mesma coisa criaçao de uma "class", que dentro dela tem duas variaveis
   float preto;   //variavel "preto" do tipo real
-  float branco;  //variavel "preto" do tipo real
+  float branco;  //variavel "branco" do tipo real
 };
 
