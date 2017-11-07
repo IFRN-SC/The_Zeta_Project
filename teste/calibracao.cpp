6356222c021170cbd72f61f217c5152108a805f9
ithaelly <ithaellymykarlla@gmail.com>
2017-11-07 12:08:00 -0300

    Fiz comentários em partes do código, explicando a sua funcionalidade

--- teste/calibracao.cpp
+++ teste/calibracao.cpp
@@ -2,135 +2,132 @@
 #include <robo_hardware2.h>
 #include <Servo.h>
 
-media_calibracao valor;
-
-void Calibracao::menu(){  //funçao principal
-  bool sair_menu = false;
-    while(!sair_menu){                                           //laço para menu principal
+void Calibracao::menu(){                                             //"menu" eh a funçao principal da classe calibraçao
+  bool sair_menu = false;                                            //variavel"sair_menu" recebeu falso e retorna um boleano
+    while(!sair_menu){                                               //laço para menu principal
       Serial.println("### DIGITE A OPCAO DESEJADA ###");
       Serial.println("            ");
       Serial.println("Calibrar SENSOR DE REFLETANCIA");
-      Serial.println("Para o ESQUERDO aperte E "); //avisa qual letra apertar para calibrar o esquerdo
+      Serial.println("Para o ESQUERDO aperte E ");               //avisa qual letra apertar para calibrar o esquerdo
       Serial.println("Para o DIREITO aperte D");                 //avisa qual letra apertar para calibrar o direito
-      Serial.println("Para o MAIS ESQUERDO aperte M");                 //avisa qual letra apertar para calibrar o mais esquerdo
-      Serial.println("Para o MAIS DIREITO aperte N");                 //avisa qual letra apertar para calibrar o mais direito
+      Serial.println("Para o MAIS ESQUERDO aperte M");           //avisa qual letra apertar para calibrar o mais esquerdo
+      Serial.println("Para o MAIS DIREITO aperte N");            //avisa qual letra apertar para calibrar o mais direito
       Serial.println("            ");
       
-      char espera_ler = esperaLer();
+      char espera_ler = esperaLer();                             //a variavel "espera_ler" que retorna um char, recebe a funçao de "esperaLer()"
       
-      switch(espera_ler){
+      switch(espera_ler){                                        //switch(escolha caso), recebe como parametro a variavel "espera_ler"
         
         case 'E':
-            calibra_refletancia_E();
+            calibra_refletancia_E();                             //caso seja "E", ele executa a funçao "calibra_refletancia_E()"
             break;
         case 'D':
-            calibra_refletancia_D();
+            calibra_refletancia_D();                             //caso seja "D", ele executa a funçao "calibra_refletancia_D()"
             break;
         case 'M':    
-            calibra_refletancia_mais_E();
+            calibra_refletancia_mais_E();                        //caso seja "M", ele executa a funçao "calibra_refletancia_mais_E()"
             break;
         case 'N':
-            calibra_refletancia_mais_D();
+            calibra_refletancia_mais_D();                        //caso seja "N", ele executa a funçao "calibra_refletancia_mais_D()"
             break;
         case 'S':
           sair_menu=true;
           
       }
     }
-
 }
 
-void Calibracao::calibra_refletancia_E(){           //laço para calibrar o sensor de refletancia esquerdo
-  Refletancia EsqDefinitivo;
-  EsqDefinitivo.preto=0;
-  EsqDefinitivo.branco=100;
-  sair_menu_calibra = false;
+void Calibracao::calibra_refletancia_E(){           //"calibra_refletancia_E()" eh a funçao da classe "calibracao", para calibrar o sensor de refletancia ESQUERDO
+  Refletancia EsqDefinitivo;                                      //struct do sensor de refletancia esquerdo, que dentro dela tem as variaveis preto e branco
+  EsqDefinitivo.preto=0;                                          //a variavel "preto" do sensor de refletancia esquerdo, recebe como valor inicial "0"
+  EsqDefinitivo.branco=100;                                       //a variavel "branco" do sensor de refletancia esquerdo, recebe como valor inicial "100"
+  sair_menu_calibra = false;                                      //a variavel "sair_menu_calibra" recebe "false"
   
-  while(!sair_menu_calibra){
+  while(!sair_menu_calibra){                                      //enquanto for diferente de "sair_menu_calibra", ou seja, o contrario, ele vai continuar a executar o laço
     Serial.println("CALIBRACAO DO SENSOR ESQUERDO");
-    calibraSensor(robo.lerSensorLinhaEsq(), escolhaCor(), EsqDefinitivo);
+    calibraSensor(robo.lerSensorLinhaEsq(), escolhaCor(), EsqDefinitivo); //executa a funçao "calibraSensor", e recebe como parametro a funçao que ler o sensor,a funçao de escolher entre branco e preto, e a variavel com nome do sensor
   }
   
-  media_esq= (EsqDefinitivo.preto + EsqDefinitivo.branco) * 0.5;
-  Serial.print("Valor Calibrado no sensor Esquerdo ");
-  Serial.println(media_esq); 
+  media_esq= (EsqDefinitivo.preto + EsqDefinitivo.branco) * 0.5; //"media_esq" calcula a media do sensor esquerdo
+  Serial.print("Valor Calibrado no sensor Esquerdo ");          
+  Serial.println(media_esq);                                    //imprimindo a media calculada
   Serial.println("           ");
   
-  valor.refletancia_esq= media_esq;
-  robo.salvarCalibracao(valor);
+  valor.refletancia_esq= media_esq;                             //objeto "valor" da classe "calibracao_dados" para armazenar na EEPROM do arduino, "refletancia_esq" recebe "media_esq"
+  robo.salvarCalibracao(valor);                                 //funçao "salvarCalibracao" recebe como parametro o objeto "valor"
 }
 
-void Calibracao::calibra_refletancia_D(){           //laço para calibrar o sensor de refletancia esquerdo
-  Refletancia DirDefinitivo;
-  DirDefinitivo.preto=0;
-  DirDefinitivo.branco=100;
-  sair_menu_calibra = false;
+void Calibracao::calibra_refletancia_D(){               //"calibra_refletancia_D()" eh a funçao da classe "calibracao", para calibrar o sensor de refletancia DIREITO
+  Refletancia DirDefinitivo;                                    //struct do sensor de refletancia direito, que dentro dela tem as variaveis preto e branco
+  DirDefinitivo.preto=0;                                        //a variavel "preto" do sensor de refletancia direito, recebe como valor inicial "0"
+  DirDefinitivo.branco=100;                                     //a variavel "branco" do sensor de refletancia direito, recebe como valor inicial "100"
+  sair_menu_calibra = false;                                    //a variavel "sair_menu_calibra" recebe "false"
   
-  while(!sair_menu_calibra){
+  while(!sair_menu_calibra){                                    //enquanto for diferente de "sair_menu_calibra", ele vai continuar a executar o laço
     Serial.println("CALIBRACAO DO SENSOR DIREITO");
-    calibraSensor(robo.lerSensorLinhaDir(), escolhaCor(), DirDefinitivo);
+    calibraSensor(robo.lerSensorLinhaDir(), escolhaCor(), DirDefinitivo); //executa a funçao "calibraSensor", e recebe como parametro a funçao que ler o sensor,a funçao de escolher entre branco e preto, e a variavel com nome do sensor
   }
   
-  media_dir= (DirDefinitivo.preto + DirDefinitivo.branco) * 0.5;
+  media_dir= (DirDefinitivo.preto + DirDefinitivo.branco) * 0.5;  //"media_dir" calcula a media do sensor direito
   Serial.print("Valor Calibrado no sensor Direito ");
-  Serial.println(media_dir); 
+  Serial.println(media_dir);                                   //imprimindo a media calculada
   Serial.println("           ");
   
-  valor.refletancia_dir= media_dir;
-  robo.salvarCalibracao(valor);
+  valor.refletancia_dir= media_dir;                            //objeto "valor" da classe "calibracao_dados" para armazenar na EEPROM do arduino, "refletancia_dir" recebe "media_dir"
+  robo.salvarCalibracao(valor);                                //funçao "salvarCalibracao" recebe como parametro o objeto "valor"
 }
 
-void Calibracao::calibra_refletancia_mais_E(){           //laço para calibrar o sensor de refletancia esquerdo
-  Refletancia EsqDefinitivo;
-  EsqDefinitivo.preto=0;
-  EsqDefinitivo.branco=100;
-  sair_menu_calibra = false;
+void Calibracao::calibra_refletancia_mais_E(){       //"calibra_refletancia_mais_E()" eh a funçao da classe "calibracao", para calibrar o sensor de refletancia MAIS ESQUERDO
+  Refletancia EsqDefinitivo;                                   //struct do sensor de refletancia mais esquerdo, que dentro dela tem as variaveis preto e branco 
+  EsqDefinitivo.preto=0;                                       //a variavel "preto" do sensor de refletancia mais esquerdo, recebe como valor inicial "0"
+  EsqDefinitivo.branco=100;                                    //a variavel "branco" do sensor de refletancia mais esquerdo, recebe como valor inicial "100"
+  sair_menu_calibra = false;                                   //a variavel "sair_menu_calibra" recebe "false"
   
-  while(!sair_menu_calibra){
+  while(!sair_menu_calibra){                                   //enquanto for diferente de "sair_menu_calibra", ele vai continuar a executar o laço
     Serial.println("CALIBRACAO DO SENSOR MAIS ESQUERDO");
-    calibraSensor(robo.lerSensorLinhaEsq(), escolhaCor(), EsqDefinitivo);
+    calibraSensor(robo.lerSensorLinhaEsq(), escolhaCor(), EsqDefinitivo); //executa a funçao "calibraSensor", e recebe como parametro a funçao que ler o sensor,a funçao de escolher entre branco e preto, e a variavel com nome do sensor
   }
   
-  media_mais_esq= (EsqDefinitivo.preto + EsqDefinitivo.branco) * 0.5;
+  media_mais_esq= (EsqDefinitivo.preto + EsqDefinitivo.branco) * 0.5;   //"media_mais_esq" calcula a media do sensor mais esquerdo
   Serial.print("Valor Calibrado no sensor Mais Esquerdo ");
-  Serial.println(media_mais_esq); 
+  Serial.println(media_mais_esq);                              //imprimindo a media calculada
   Serial.println("           ");
   
-  valor.refletancia_mais_esq= media_mais_esq;
-  robo.salvarCalibracao(valor);  
+  valor.refletancia_mais_esq= media_mais_esq;                  //objeto "valor" da classe "calibracao_dados" para armazenar na EEPROM do arduino, "refletancia_mais_esq" recebe "media_mais_esq"
+  robo.salvarCalibracao(valor);                                //funçao "salvarCalibracao" recebe como parametro o objeto "valor"
 }
 
-void Calibracao::calibra_refletancia_mais_D(){           //laço para calibrar o sensor de refletancia esquerdo
-  Refletancia DirDefinitivo;
-  DirDefinitivo.preto=0;
-  DirDefinitivo.branco=100;
-  sair_menu_calibra = false;
+void Calibracao::calibra_refletancia_mais_D(){       //"calibra_refletancia_mais_D()" eh a funçao da classe "calibracao", para calibrar o sensor de refletancia MAIS DIREITO
+  Refletancia DirDefinitivo;                                  //struct do sensor de refletancia mais direito, que dentro dela tem as variaveis preto e branco
+  DirDefinitivo.preto=0;                                      //a variavel "preto" do sensor de refletancia mais direito, recebe como valor inicial "0"
+  DirDefinitivo.branco=100;                                   //a variavel "branco" do sensor de refletancia mais direito, recebe como valor inicial "100"
+  sair_menu_calibra = false;                                  //a variavel "sair_menu_calibra" recebe "false"
   
-  while(!sair_menu_calibra){
+  while(!sair_menu_calibra){                                  //enquanto for diferente de "sair_menu_calibra", ele vai continuar a executar o laço
     Serial.println("CALIBRACAO DO SENSOR MAIS DIREITO");
-    calibraSensor(robo.lerSensorLinhaDir(), escolhaCor(), DirDefinitivo);
+    calibraSensor(robo.lerSensorLinhaDir(), escolhaCor(), DirDefinitivo); //executa a funçao "calibraSensor", e recebe como parametro a funçao que ler o sensor,a funçao de escolher entre branco e preto, e a variavel com nome do sensor
   }
   
-  media_mais_dir= (DirDefinitivo.preto + DirDefinitivo.branco) * 0.5;
+  media_mais_dir= (DirDefinitivo.preto + DirDefinitivo.branco) * 0.5;  //"media_mais_dir" calcula a media do sensor mais direito
   Serial.print("Valor Calibrado no sensor Mais Direito ");
-  Serial.println(media_mais_dir); 
+  Serial.println(media_mais_dir);                             //imprimindo a media calculada
   Serial.println("           ");
   
-  valor.refletancia_mais_dir= media_mais_dir;
-  robo.salvarCalibracao(valor);
+  valor.refletancia_mais_dir= media_mais_dir;                  //objeto "valor" da classe "calibracao_dados" para armazenar na EEPROM do arduino, "refletancia_mais_dir" recebe "media_mais_dir"
+  robo.salvarCalibracao(valor);                                //funçao "salvarCalibracao" recebe como parametro o objeto "valor"
 }
 
-char Calibracao::escolhaCor(){
-  Serial.println("Para calibrar no PRETO aperte P");
+char Calibracao::escolhaCor(){                                 //funçao "escolhaCor" da classe "Calibracao"
+  Serial.println("Para calibrar no PRETO aperte P");           
   Serial.println("Para calibrar no BRANCO aperte B");
   Serial.println("Para sair aperte S");
   Serial.println("           "); 
-   char espera_ler = esperaLer();
+   char espera_ler = esperaLer();                             //char "espera_ler" armazena a escolha da pessoa. e recebe a funçao "esperaLer", ou seja, espera ate digitarem alguma escolha
    
-   return espera_ler;
+   return espera_ler;                                         //retorna a opçao escolhida, ou seja, a letra
 }
 
-bool Calibracao::calibraSensor(float valorAtual, char espera_ler, Refletancia &Definitivo){
+bool Calibracao::calibraSensor(float valorAtual, char espera_ler, Refletancia &Definitivo){//funçao "calibraSensor" da classe "Calibracao", "valorAtual" recebe o valor lido na hora pelo sensor, "espera_ler" retorna a cor escolhida, a Struct Refletancia retorna qual o sensor
    sair_menu_calibra = false;
     switch(espera_ler){
       
