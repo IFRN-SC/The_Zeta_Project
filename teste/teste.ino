eeb5c0d87a6c4173c4fbcc4f10c079af49c1ec50
ithaelly <ithaellymykarlla@gmail.com>
2018-09-21 20:54:41 -0300

    Criada o timeout para a calibração: Criação de um contador que após um determinado tempo se não apetar no botão para  fazer a calibração, ele começa o código de execução normal do robô.

--- teste/teste.ino
+++ teste/teste.ino
@@ -9,6 +9,21 @@ void setup(){
   pinMode(LED_BUILTIN, OUTPUT);
   robo.configurar(false);
   Serial.begin(9600);
+  //repetir 10 vezes
+  for(int i=0; i<=10; i++){
+    if(robo.botao1Pressionado()){
+      robo.ligarLed(1);
+      delay(100);
+      robo.desligarLed(1);
+      delay(100);
+      escolha.calibrar(true);
+      break;
+    }
+    robo.ligarLed(2);
+    delay(100);
+    robo.desligarLed(2);
+    delay(100);
+  }
   escolha.calibrar(false);
 }
 
