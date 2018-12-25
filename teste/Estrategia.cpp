f698b3b2e537b674a114f305d97255c2405a597d
Leticia <leticiassantos15@outlook.com>
2018-12-25 17:21:00 -0300

    Tirando espaços desnecessários.

--- teste/Estrategia.cpp
+++ teste/Estrategia.cpp
@@ -12,7 +12,6 @@ Estrategia::Estrategia(){
   valor_sensor_sonar_lateral_Direito = 100000000;
 }
 
-
 void Estrategia::calibrar(boolean realizarCalibracao){        //"calibrar" eh funçao da classe "Estrategia"
   if(realizarCalibracao){                                     //se realizarCalibraçao ele vai chamar a funçao calibrar
       sensor.calibrar();                                      //objeto "sensor" da classe "sensores", chama a funçao "calibrar"
