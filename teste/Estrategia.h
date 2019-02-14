004d799bf2068ba0689a8a0a0d83eae700ce7911
Leticia <leticiassantos15@outlook.com>
2019-02-13 21:20:10 -0300

    Include da classe GARRA, nesta classe para fazer o uso da mesma, e criando funções, como: seguir_linha, testar_verde.

--- teste/Estrategia.h
+++ teste/Estrategia.h
@@ -2,6 +2,7 @@
 #include <Servo.h>
 #include "sensores.h"
 #include "sensor_cor.h"
+#include "Garra.h"
 
 class Estrategia{              //classe Estrategia
   public:                      //todos as variaveis e funçoes publicas pode ser acessados em outra classe
@@ -10,13 +11,18 @@ class Estrategia{              //classe Estrategia
     void calibrar(boolean realizarCalibracao);      //"calibrar" eh uma funçao criada para decidir caso queira calibrar ou nao o robo
     void alinhaObstaculo();
     void contornarObstaculo();
+    void testar_verde();
+    void seguir_linha();
     void girar_esquerdo_verde();
     void girar_direito_verde();
     void subir_rampa();
+    void resgate();
     
   private:                    //todos as variaveis funçoes privadas s podem ser acessadas na classe em que foi criada
     sensores sensor;            //"sensor"  o objeto da classe "sensores". objeto criado para usar as funçoes criadas dentro da classe "sensores"
     float valor_sensor_sonar_frontal;
     float valor_sensor_sonar_lateral_Esquerdo;
     float valor_sensor_sonar_lateral_Direito;
+
+    Garra garra;
 };
