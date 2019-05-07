864d687977e9cf99c2c17024098b89fbdf200a44
Leticia <leticiassantos15@outlook.com>
2019-05-07 05:50:04 -0300

    criação da classs resgate.cpp

---
+++ teste/Resgate.cpp
@@ -0,0 +1,21 @@
+#include "Resgate.h"
+
+Resgate::Resgate(){}
+
+void Resgate::alinhar (){
+    robo.acionarMotores(-40,-40);
+    delay (1000);
+    robo.acionarMotores(40,40);
+    delay (200);
+    robo.acionarMotores(-40,-40);
+    delay (700);
+    robo.acionarMotores(40,40);
+    delay (150);
+    robo.acionarMotores(-40,-40);
+    delay (1000);
+    robo.acionarMotores(0,0);
+    delay (2000);
+}
+
+
+
